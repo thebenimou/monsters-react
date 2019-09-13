5d2e30f67f9f928c5dc4a3191b3c7fe3a3286280
Benjamin Ejzenberg <benjamin.ejzenberg@gmail.com>
2019-09-13 08:43:47 -0400

    adding files for github pages

---
+++ src/App.js
@@ -0,0 +1,62 @@
+  import React, {Component} from 'react';
+  import {CardList} from "./components/card-list/card-list.component"
+  import './App.css';
+  import {SearchBox} from "./components/search-box/search-box.component"
+
+
+// pour lancer l'appli en dev 
+// cd monsters
+// npm start
+
+// pour publier sur github
+// cd monsters
+// git init
+// git remote add origin git@github.com:thebenimou/monsters-react.git
+// npm install gh-pages --save
+// modifier le package.json :
+//          "homepage": "https://thebenimou.github.io/monsters-react",
+//          ajouter  dans scripts "predeploy": "yarn build", "deploy":"gh-pages -d build"
+// yarn build
+
+  class App extends Component {
+
+    constructor(){
+      super();
+      this.state = {
+          monsters: [],
+          searchField: ""
+      }
+    }
+
+    render(){
+      // on recréé des variables locales
+      const { monsters, searchField } = this.state;
+      const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+      return (
+        <div className="App">
+        <h1>Monsters App</h1>
+        <SearchBox
+          placeholder='search monsters'
+          handleChange={e => this.setState({searchField:e.target.value})}
+        />
+        <CardList monsters={filteredMonsters}>
+        </CardList> 
+      
+        </div>
+      ); 
+    }
+
+    componentDidMount(){
+      fetch("http://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({monsters:users}))
+    }
+
+
+
+
+  }
+
+  export default App;
