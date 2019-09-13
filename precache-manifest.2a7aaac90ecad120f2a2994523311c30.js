35f69d7b1c3d6dfeb59d640496395e8e9c762045
Benjamin Ejzenberg <benjamin.ejzenberg@gmail.com>
2019-09-13 09:03:57 -0400

    Updates

---
+++ precache-manifest.2a7aaac90ecad120f2a2994523311c30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533cc105c1046339df761a7817e53f3b",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "eb6f9deb185072e367d1",
+    "url": "/monsters-react/static/css/main.0401143f.chunk.css"
+  },
+  {
+    "revision": "783bb2d55f15e42d1998",
+    "url": "/monsters-react/static/js/2.f3e65479.chunk.js"
+  },
+  {
+    "revision": "eb6f9deb185072e367d1",
+    "url": "/monsters-react/static/js/main.9687fb19.chunk.js"
+  },
+  {
+    "revision": "8f5b4058548bab5621ec",
+    "url": "/monsters-react/static/js/runtime~main.91ce7a21.js"
+  }
+]);
